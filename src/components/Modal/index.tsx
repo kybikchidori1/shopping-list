607521f8c6757c8cf4dc799fd7658ddb122cb24d
Roman <kybik_rybik1994@mail.ru>
2022-08-02 17:29:32 +0300

    complete project

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -2,41 +2,41 @@ import { HTMLProps } from "react";
 import "./index.scss";
 
 interface HeaderProps extends HTMLProps<HTMLDivElement> {
-    title: string;
-    actions?: JSX.Element;
+  title: string;
+  actions?: JSX.Element;
 }
 
 const Wrapper = ({
-    children,
-    className,
+  children,
+  className,
 }: HTMLProps<HTMLDivElement>): JSX.Element => (
-    <div className={`modal__wrapper flex-center ${className ?? ""}`}>
-        {children}
-    </div>
+  <div className={`modal__wrapper flex-center ${className ?? ""}`}>
+    {children}
+  </div>
 );
 
 const Header = ({ title, actions, className }: HeaderProps): JSX.Element => (
-    <div className={`modal__header flex-center ${className ?? ""}`}>
-        <div className="modal__title">{title}</div>
-        <div>{actions ?? actions}</div>
-    </div>
+  <div className={`modal__header flex-center ${className ?? ""}`}>
+    <div className="modal__title">{title}</div>
+    <div>{actions ?? actions}</div>
+  </div>
 );
 
 const Body = ({
-    children,
-    className,
-    ...props
+  children,
+  className,
+  ...props
 }: HTMLProps<HTMLDivElement>): JSX.Element => (
-    <div className={`modal__body flex-center ${className ?? ""}`} {...props}>
-        {children}
-    </div>
+  <div className={`modal__body  ${className ?? ""}`} {...props}>
+    {children}
+  </div>
 );
 
 const Footer = ({
-    children,
-    className,
+  children,
+  className,
 }: HTMLProps<HTMLDivElement>): JSX.Element => (
-    <div className={`modal__footer ${className ?? ""}`}>{children}</div>
+  <div className={`modal__footer ${className ?? ""}`}>{children}</div>
 );
 
 const Modal = { Wrapper, Header, Body, Footer };
